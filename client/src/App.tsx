7fe113e3f3deb73f8d968bd06546a06aaa2ffb56
Mikko Leppänen <mleppan23@gmail.com>
2021-11-03 09:59:00 +0200

    frontend-initial-setup; initial setup for the client code base.
    Signed-off-by: Mikko Leppänen <mleppan23@gmail.com>

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+function App() {
+  return (
+    <div>
+      <Typography>Testing...</Typography>
+    </div>
+  );
+}
+
+export default App;
