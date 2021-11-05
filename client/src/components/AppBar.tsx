1ef6114a181af380258e29214f5c36ab698f7787
Mikko Leppänen <mleppan23@gmail.com>
2021-11-05 18:37:49 +0200

    feature/file-menu-handling; Changes: * Added File menu item for importing external source code and support for copying inline code to clipboard.
    Signed-off-by: Mikko Leppänen <mleppan23@gmail.com>

--- client/src/components/AppBar.tsx
+++ client/src/components/AppBar.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
 import AppBarTitle from "./AppBarTitle";
+import FileLoadMenu from "./FileLoadMenu";
+import { Divider, Toolbar } from "@mui/material";
 
 export default function NavBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar elevation={0} className="Appbar">
+      <AppBar elevation={0} position="static" className=" Appbar">
         <Toolbar>
           <AppBarTitle />
+          <FileLoadMenu />
+          <Divider
+            orientation="vertical"
+            flexItem
+            style={{ marginLeft: "10px" }}
+          />
         </Toolbar>
       </AppBar>
     </Box>
---
+++ client/src/components/FileLoadMenu.tsx
@@ -0,0 +1,111 @@
+import React from "react";
+import Button from "@mui/material/Button";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import {
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  MenuItem,
+  MenuList,
+} from "@mui/material";
+import FileUploadIcon from "@mui/icons-material/FileUpload";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+
+export default function FileLoadMenu() {
+  const [anchorEl, setAnchorEl] = React.useState<HTMLButtonElement | null>(
+    null
+  );
+
+  const handleMenu = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    if (event) {
+      setAnchorEl(event.currentTarget);
+    }
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const fileUploadSelectHandler = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (event.target.files) {
+      fileUploadHandler(event.target.files[0]);
+    }
+  };
+
+  const fileCopySelectHandler = () => {
+    console.log("File copied to...");
+    handleClose();
+  };
+
+  const fileUploadHandler = (file: File) => {
+    const reader = new FileReader();
+
+    reader.readAsText(file);
+
+    reader.onload = function () {
+      console.log(reader.result);
+    };
+
+    reader.onerror = function () {
+      console.log(reader.error);
+    };
+  };
+
+  return (
+    <div>
+      <Button
+        variant="contained"
+        style={{ marginLeft: "30px", backgroundColor: "#64748B" }}
+        endIcon={<ArrowDownwardIcon />}
+        onClick={handleMenu}
+      >
+        File
+      </Button>
+      <Menu
+        id="menu-appbar"
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuList>
+          <input
+            accept=".go"
+            style={{ display: "none", width: "100%" }}
+            id="file-upload"
+            type="file"
+            onChange={fileUploadSelectHandler}
+          />
+          <label htmlFor="file-upload">
+            <MenuItem onClick={handleClose}>
+              <ListItemIcon>
+                <FileUploadIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText>Import</ListItemText>
+            </MenuItem>
+          </label>
+          <label htmlFor="copy">
+            <MenuItem onClick={fileCopySelectHandler}>
+              <ListItemIcon>
+                <ContentCopyIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText>Copy </ListItemText>
+            </MenuItem>
+          </label>
+        </MenuList>
+      </Menu>
+    </div>
+  );
+}
