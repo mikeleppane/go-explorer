e69f598565ed88cdf83c183c7ebcc4f0c7ca120b
Mikko Leppänen <mleppan23@gmail.com>
2021-12-14 08:59:50 +0200

    Reformatted code templates.
    Signed-off-by: Mikko Leppänen <mleppan23@gmail.com>

--- client/src/config/codeTemplates.ts
+++ client/src/config/codeTemplates.ts
@@ -1,77 +1,77 @@
 export const defaultCode = `
-  // You can edit this code!
-  // Click here and start typing.
-  package main
+// You can edit this code!
+// Click here and start typing.
+package main
 
-  import "fmt"
+import "fmt"
 
-  func main() {
-      fmt.Println("Hello,世界")
-  }
+func main() {
+  fmt.Println("Hello,世界")
+}
 `;
 
 export const testingCode = `
-  package main
-  
-  import (
-      "fmt"
-      "testing"
-  )
-  
-  func IntMin(a, b int) int {
-      if a < b {
-          return a
-      }
-      return b
+package main
+
+import (
+  "fmt"
+  "testing"
+)
+
+func IntMin(a, b int) int {
+  if a < b {
+    return a
   }
-  
-  func TestIntMinBasic(t *testing.T) {
-      ans := IntMin(2, -2)
-      if ans != -2 {
-          t.Errorf("IntMin(2, -2) = %d; want -2", ans)
-      }
+  return b
+}
+
+func TestIntMinBasic(t *testing.T) {
+  ans := IntMin(2, -2)
+  if ans != -2 {
+    t.Errorf("IntMin(2, -2) = %d; want -2", ans)
   }
+}
 `;
 
 export const benchmarkCode = `
-  package main
-  
-  import (
-      "fmt"
-      "testing"
-  )
-  
-  func IntMin(a, b int) int {
-      if a < b {
-          return a
-      }
-      return b
+package main
+
+import (
+  "fmt"
+  "testing"
+)
+
+func IntMin(a, b int) int {
+  if a < b {
+    return a
   }
-  
-  func BenchmarkIntMin(b *testing.B) {
-    for i := 0; i < b.N; i++ {
-        IntMin(1, 2)
-    }
+  return b
+}
+
+func BenchmarkIntMin(b *testing.B) {
+  for i := 0; i < b.N; i++ {
+    IntMin(1, 2)
   }
+}
 `;
 
 export const concurrencyCode = `
-  package main
+package main
 
-  import (
-      "fmt"
-      "time"
-  )
+import (
+  "fmt"
+  "time"
+)
 
-  func say(s string) {
-      for i := 0; i < 5; i++ {
-          time.Sleep(100 * time.Millisecond)
-          fmt.Println(s)
-      }
+func say(s string) {
+  for i := 0; i < 5; i++ {
+    time.Sleep(100 * time.Millisecond)
+    fmt.Println(s)
   }
+}
 
-  func main() {
-      go say("world")
-      say("hello")
-  }
+func main() {
+  go say("world")
+  say("hello")
+}
 `;
