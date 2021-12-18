d8f97f9d6a37f5433ef1578e005b5c1072ba6b14
Mikko Leppänen <mleppan23@gmail.com>
2021-12-18 15:24:28 +0200

    feature/CICD-pipeline: testing pipeline, 1 attempt
    Signed-off-by: Mikko Leppänen <mleppan23@gmail.com>

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,7 @@
 import express = require("express");
-import logger from "./utils/logging";
 import { setupRoutes } from "./startup/routes";
-import http from "http";
+import { start } from "./startup/start";
 
 const app = express();
 setupRoutes(app);
-const server = http.createServer(app);
-const port = process.env.PORT || 5000;
-server.listen(port, () => logger.info(`Listening on port ${port}...`));
+start(app);
